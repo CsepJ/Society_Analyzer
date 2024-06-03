c42165c22a206fa1d65203533abadc7bb089c0ef
SepJ <sepj0921@gmail.com>
2024-06-03 21:35:50 +0900

    AI Constructed

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ from matplotlib import pyplot as plt
 
 data_dir = Path(f"./{API_Request.folderName}")
 
-
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(64, activation="tanh"),
+    tf.keras.layers.Dense(128, activation="tanh"),
+    tf.keras.layers.Dense()
+])
